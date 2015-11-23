2d9d460e04cf19d3d3636d10b7e6a2311c412f8d
Eric Tsang <surplus.et@gmail.com>
2015-11-23 03:48:05 -0800

    questions ii_20 to ii_21
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/ii_collections/_21_Partition_.kt
+++ src/ii_collections/_21_Partition_.kt
@@ -12,5 +12,6 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    val (result) = customers.partition{it.orders.count{!it.isDelivered} > it.orders.count()/2}
+    return result.toSet()
 }
