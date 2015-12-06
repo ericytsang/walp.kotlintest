cba9e1bebdfafe8cbda6f54bc1f3f54f4ff4ba0f
Eric Tsang <surplus.et@gmail.com>
2015-12-06 02:25:35 -0800

    work on package iii_conventions, finished problems 25 - 26
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,19 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int):Comparable<MyDate>
+{
+    override fun compareTo(other:MyDate):Int
+    {
+        when
+        {
+            year != other.year -> return year-other.year
+            month != other.month -> return month-other.month
+            else -> return dayOfMonth-other.dayOfMonth
+        }
+    }
+}
 
 enum class TimeInterval {
     DAY,
@@ -8,4 +21,22 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(public val start: MyDate, public val end: MyDate)
+class DateRange(public val start: MyDate, public val end: MyDate):Iterable<MyDate>
+{
+    override fun iterator():Iterator<MyDate>
+    {
+        return MyIterator()
+    }
+
+    inner class MyIterator:Iterator<MyDate>
+    {
+        private var nextDate = start
+        override fun next():MyDate
+        {
+            val result = nextDate
+            nextDate = nextDate.nextDay()
+            return result
+        }
+        override fun hasNext():Boolean = nextDate <= end
+    }
+}
